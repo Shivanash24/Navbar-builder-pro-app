d9e979b3d050e742e5c341d6157bda63f48aa492
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-05-11 10:48:39 +0530

    Push remaining code

--- prisma/migrations/20260428055554_navbar/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Navbar" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
-    "designId" INTEGER NOT NULL,
-    "menu" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
